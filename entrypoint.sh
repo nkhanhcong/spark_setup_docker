7a45373af4bb474ac69f98ad88f7cdf0955a038b
congnk <nkhanhcong@gmail.com>
2024-05-16 13:23:14 +0700

    setup spark submit with docker, try to set up logs spark events

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-if [ "$SPARK_MODE" == "master" ]; then
-    # Start Spark master
-    $SPARK_HOME/sbin/start-master.sh -h 0.0.0.0
-elif [ "$SPARK_MODE" == "worker" ]; then
-    # Start Spark worker and link it to the master
-    $SPARK_HOME/sbin/start-worker.sh $SPARK_MASTER_URL
-elif ["$SPARK_MODE" == "history"]; then
-    #Start Spark history server
-    $SPARK_HOME/sbin/start-history-server.sh
+echo "Starting entrypoint script..."
+
+# Set Spark Event Logging Configuration based on environment variables
+if [ "$SPARK_EVENT_LOG_ENABLED" == "true" ]; then
+    SPARK_LOGGING_OPTS="
+        --conf spark.eventLog.enabled=true
+        --conf spark.eventLog.dir=$SPARK_EVENT_LOG_DIR
+    "
 else
-    echo "Please specify a valid SPARK_MODE: master or worker"
+    SPARK_LOGGING_OPTS=""
+fi
+
+if [ "$SPARK_MODE" == "master" ]; then
+    echo "Starting Spark master..."
+    $SPARK_HOME/sbin/start-master.sh -h 0.0.0.0
+    echo "Spark master started."
+elif [ "$SPARK_MODE" == "worker" ]; then
+    echo "Starting Spark worker..."
+    echo "Connecting to Spark master at $SPARK_MASTER_URL"
+    $SPARK_HOME/sbin/start-worker.sh $SPARK_MASTER_URL $SPARK_LOGGING_OPTS
+    echo "Spark worker started."
+elif [ "$SPARK_MODE" == "history" ]; then
+    echo "Starting Spark history server..."
+    $SPARK_HOME/sbin/start-history-server.sh $SPARK_LOGGING_OPTS
+    echo "Spark history server started."
+else
+    echo "Please specify a valid SPARK_MODE: master, worker, or history"
     exit 1
 fi
 
 # Keep the container running
+echo "Entrypoint script completed. Container will keep running."
 tail -f /dev/null
